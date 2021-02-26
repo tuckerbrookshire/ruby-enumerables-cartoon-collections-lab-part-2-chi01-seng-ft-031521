af2a2ecb1457704f9bdada82b56ff9d370813137
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 23:17:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find? do |valid_calls|
+  planeteer_calls.find? do |element|
+   valid_calls[element]
     
   end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
