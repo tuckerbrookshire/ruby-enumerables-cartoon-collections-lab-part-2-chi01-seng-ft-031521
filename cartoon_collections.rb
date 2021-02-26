88e19e61578fcd98937d59035343023363288c66
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 22:48:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.each do |element|
-    char_val == element.size > 4
-    char_val.any?
-    
+  planeteer_calls.any? do |element|
+     element.size > 4
   end
   
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
