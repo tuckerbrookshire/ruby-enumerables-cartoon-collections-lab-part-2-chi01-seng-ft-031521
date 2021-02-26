5f6bde312114794943a6c8a2de108c0661a45ee6
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 22:52:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.find do |valid_calls|
+  planeteer_calls.find_all do |valid_calls|
     
   
   
